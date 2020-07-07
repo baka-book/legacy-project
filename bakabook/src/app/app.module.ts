f3333c067e8900840cf3205217f6516c8b852cc3
ahmed-fenni <ahmedfenny8@gmail.com>
2020-07-07 18:51:13 +0200

    working on userProfile

--- bakabook/src/app/app.module.ts
+++ bakabook/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule , routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
