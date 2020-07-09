24ad7deec11a3c5523edb8941ba3d04854f64195
aliSnakesou <lkollasfbt@gmail.com>
2020-07-09 11:46:24 +0100

    adding dynaming comment field

--- src/app/bookdetails/bookdetails.component.ts
+++ src/app/bookdetails/bookdetails.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-
+import $ from "jquery";
 @Component({
   selector: "app-bookdetails",
   templateUrl: "./bookdetails.component.html",
@@ -9,4 +9,32 @@ export class BookdetailsComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  generate() {
+    $(function () {
+      $(document)
+        .on("click", ".btn-add", function (e) {
+          e.preventDefault();
+
+          var controlForm = $(".controls form:first"),
+            currentEntry = $(this).parents(".entry:first"),
+            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+          newEntry.find("input").val("");
+          controlForm
+            .find(".entry:not(:last) .btn-add")
+            .removeClass("btn-add")
+            .addClass("btn-remove")
+            .removeClass("btn-success")
+            .addClass("btn-danger")
+            .html('<span ">Delete Comment</span>');
+        })
+        .on("click", ".btn-remove", function (e) {
+          $(this).parents(".entry:first").remove();
+
+          e.preventDefault();
+          return false;
+        });
+    });
+  }
 }
